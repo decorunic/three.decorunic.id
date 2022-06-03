03249435ee3b64fa921593f6aedbc2a2b4472c65
virgiawankusuma <virgiawan150@gmail.com>
2022-06-03 15:49:21 +0700

    seting up routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,33 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+Route::get('/', function () {
+    return 'dashboard';
+});
+
+Route::get('/products/{id}', function ($id) {
+    return 'Product '.$id;
+});
+
+Route::get('/products/list', function () {
+    return 'Product List';
+});
+
+Route::get('/products/add', function () {
+    return 'Product Add';
+});
+
+Route::get('/products/save', function () {
+    return 'Product Save';
+});
+
+Route::get('/products/edit/{id}', function ($id) {
+    return 'Product Edit '.$id;
+});
+
+Route::get('/products/update/{id}', function ($id) {
+    return 'Product Update '.$id;
 });
